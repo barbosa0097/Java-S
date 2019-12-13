ddf97bd6669ebe39a083512b70b590a665bbe10f
David Barbosa <barbosa0097@gmail.com>
2019-12-12 20:54:14 -0800

    add Java S numbers, strings, arrays, objects

---
+++ JS/scripts.js
@@ -0,0 +1,45 @@
+// Numbers
+// numbers and be computed with +, -, *, /, %, ++ increment, --decrement
+// NaN means Not a Number
+var pizza = 5;
+var soda = 1;
+var bread = 3;
+
+var total = pizza + soda;
+
+console.log(total);
+
+/** 
+ * The comsole. log() is a function in JacaScript which is used to print any kind of varisbles defined before in it or to just print any message that needs to be displayed to the user.
+ */
+ 
+ // String
+
+ const name = 'David Barbosa';
+
+ const welcome = "Welcome to Geekwise Academy";
+
+
+
+ console.log(name);
+ console.log(welcome);
+
+ //Array
+ var fruits = ['apples', 'grapes', 'oranges', 'bananas'];
+ var rockBand = ['Pantera', 'AC DC', 'Carrot Top'];
+
+ console.log(fruits) ;
+ console.log(rockBand);
+
+ // Object
+
+ var car = {
+     type:"Fiat",
+     model:"500",
+     color:"white"
+ };
+
+ console.log(car);
+
+ // Boolean
+ // everything with a real value is true 0, null, undefined is false 
